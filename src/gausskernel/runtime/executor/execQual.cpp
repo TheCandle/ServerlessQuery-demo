f76b7a9f93388886389d7e32c9eb3b065dc04552
totaj <totaj@qq.com>
2025-08-28 11:00:43 +0800

    plugin performance optimize

--- src/gausskernel/runtime/executor/execQual.cpp
+++ src/gausskernel/runtime/executor/execQual.cpp
@@ -2410,7 +2410,7 @@ restart:
        fcache->setArgsValid = false;
    }
 
-    if (DB_IS_CMPT(B_FORMAT)) {
+    if (DB_IS_CMPT(B_FORMAT) && !COLLATION_HAS_INVALID_ENCODING(fcinfo->fncollation)) {
         func_encoding = get_valid_charset_by_collation(fcinfo->fncollation);
         db_encoding = GetDatabaseEncoding();
     }
@@ -2898,7 +2898,7 @@ static Datum ExecMakeFunctionResultNoSets(
        }
    }
 
-   if (DB_IS_CMPT(B_FORMAT)) {
+    if (DB_IS_CMPT(B_FORMAT) && !COLLATION_HAS_INVALID_ENCODING(fcinfo->fncollation)) {
         func_encoding = get_valid_charset_by_collation(fcinfo->fncollation);
         db_encoding = GetDatabaseEncoding();
     }
