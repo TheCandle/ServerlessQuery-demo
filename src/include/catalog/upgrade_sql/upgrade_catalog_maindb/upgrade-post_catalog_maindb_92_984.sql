b62fe55c34eebb9b0b2d14c6d9b5866459a50602
chenxiaobin19 <1025221611@qq.com>
2025-10-16 17:04:27 +0800

    修复pg_am升级元数据不一致的问题

--- src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_92_984.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_92_984.sql
@@ -157,3 +157,20 @@ CREATE OPERATOR CLASS pg_catalog.vector_cosine_ops
 	FUNCTION 1 pg_catalog.vector_negative_inner_product(vector, vector),
 	FUNCTION 2 pg_catalog.vector_norm(vector),
 	FUNCTION 4 pg_catalog.vector_norm(vector);
+
+CREATE OR REPLACE FUNCTION Update_pg_amproc_temp()
+RETURNS void AS $$
+DECLARE
+query_str text;
+BEGIN
+query_str := 'update pg_catalog.pg_am set amhandler = 8208 where amname = ''hnsw'' and amhandler = 0';
+EXECUTE(query_str);
+query_str := 'update pg_catalog.pg_am set amhandler = 8206 where amname = ''ivfflat'' and amhandler = 0';
+EXECUTE(query_str);
+query_str := 'update pg_catalog.pg_am set amhandler = 8532 where amname = ''diskann'' and amhandler = 0';
+EXECUTE(query_str);
+return;
+END; $$ LANGUAGE 'plpgsql';
+
+SELECT Update_pg_amproc_temp();
+DROP FUNCTION Update_pg_amproc_temp();
